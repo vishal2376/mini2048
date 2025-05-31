4d5866e4682c9c2ee0528ad27af5c04a6b5da9ba
Vishal Singh <vishalsingh2376@gmail.com>
2025-05-31 20:11:52 +0530

    misc: Add koin dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,11 @@ dependencies {
 	implementation(libs.androidx.ui.graphics)
 	implementation(libs.androidx.ui.tooling.preview)
 	implementation(libs.androidx.material3)
+
+	// koin
+	implementation(libs.koin.androidx.compose)
+	implementation(libs.koin.androidx.compose.navigation)
+
 	testImplementation(libs.junit)
 	androidTestImplementation(libs.androidx.junit)
 	androidTestImplementation(libs.androidx.espresso.core)
