a82f4e0c665d16df06d172a27a3c56a564b7336d
Vishal Singh <vishalsingh2376@gmail.com>
2025-05-31 20:11:29 +0530

    feat: Update Colors and Theme

--- app/src/main/java/com/vishal2376/mini2048/ui/theme/Color.kt
+++ app/src/main/java/com/vishal2376/mini2048/ui/theme/Color.kt
@@ -2,10 +2,24 @@ package com.vishal2376.mini2048.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Base theme colors
+val Primary = Color(0xFF2196F3)
+val Background = Color(0xFF000000)
+val PrimaryContainer = Color(0xFF1A1A1A)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Tile colors
+val Tile2 = Color(0xFFEEE4DA)
+val Tile4 = Color(0xFFEDE0C8)
+val Tile8 = Color(0xFFF2B179)
+val Tile16 = Color(0xFFF59563)
+val Tile32 = Color(0xFFF67C5F)
+val Tile64 = Color(0xFFF65E3B)
+val Tile128 = Color(0xFFEDCF72)
+val Tile256 = Color(0xFFEDCC61)
+val Tile512 = Color(0xFFEDC850)
+val Tile1024 = Color(0xFFEDC53F)
+val Tile2048 = Color(0xFFEDC22E)
+val TileSuper = Color(0xFF3C3A32)
+
+val TileTextDark = Color(0xFF776E65)
+val TileTextLight = Color.White
